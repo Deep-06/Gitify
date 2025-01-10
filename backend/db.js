79efdaae6f48df34e3e7394d4d5b4dbcf4d977ed
Deep-06 <shikhadeep.hzp@gmail.com>
2025-01-10 21:01:55 +0530

    backend

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-//dotenv.config();
+dotenv.config();
 // const connectDB = () =>{
 //     mongoose
 //     .connect(process.env.MONGO_URL)
