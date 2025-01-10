756a4de07764fb0d7ede9d2c55e8fb83698fe877
Deep-06 <shikhadeep.hzp@gmail.com>
2025-01-10 20:00:10 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -3,17 +3,30 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const connectDB = () => {
-  const mongoURI = process.env.MONGO_URL;
-
-   mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-   .then(() => {
-    console.log('connected to database')
-})
-.catch((err) => {
-    console.log(`error while connecting ${err}`)
-})
+const connectDB = () =>{
+    mongoose
+    .connect(process.env.MONGO_URL, {
+       dbName: "Gitify",
+    })
+    .then(() => {
+        console.log('connected to database')
+    })
+    .catch((err) => {
+        console.log(`error while connecting ${err}`)
+    })
 }
+
+// const connectDB = () => {
+//   const mongoURI = process.env.MONGO_URL;
+
+//    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//    .then(() => {
+//     console.log('connected to database')
+// })
+// .catch((err) => {
+//     console.log(`error while connecting ${err}`)
+// })
+// }
 
 module.exports={
   connectDB
