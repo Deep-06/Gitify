cc687e6d4cd4e80a982d9dcbd2cc57567c39f36b
Deep-06 <shikhadeep.hzp@gmail.com>
2025-01-10 20:59:35 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,30 +1,32 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config();
-
-const connectDB = () =>{
-    mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log('connected to database')
-    })
-    .catch((err) => {
-        console.log(`error while connecting ${err}`)
-    })
-}
-
-// const connectDB = () => {
-//   const mongoURI = process.env.MONGO_URL;
-
-//    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//    .then(() => {
-//     console.log('connected to database')
-// })
-// .catch((err) => {
-//     console.log(`error while connecting ${err}`)
-// })
+//dotenv.config();
+// const connectDB = () =>{
+//     mongoose
+//     .connect(process.env.MONGO_URL)
+//     .then(() => {
+//         console.log('connected to database')
+//     })
+//     .catch((err) => {
+//         console.log(`error while connecting ${err}`)
+//     })
 // }
+
+const connectDB = async () => {
+  try {
+    console.log("Connecting to MongoDB URI:", process.env.MONGO_URL); // Debug log
+    if (!process.env.MONGO_URL) {
+      throw new Error("MONGO_URL is not defined in environment variables");
+    }
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('Connected to database');
+  } catch (err) {
+    console.error('Error while connecting to MongoDB:', err.message);
+    throw err; 
+  }
+};
+
 
 module.exports={
   connectDB
